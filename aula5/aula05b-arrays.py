aad5b466cf12d47ec510e7e8a531434a781cce6b
luquetabertolini <lubertolini1104@gmail.com>
2026-05-05 03:29:45 -0300

    oiii

---
+++ aula5/aula05b-arrays.py
@@ -0,0 +1,21 @@
+lista_frutas = ["Morango", "Abacaxi", "Maracujá"]
+
+# lista
+print(lista_frutas[0])
+print(lista_frutas[1])
+print(lista_frutas[2])
+
+# add no fim da lista
+lista_frutas.append("Banana")
+print(lista_frutas[3])
+
+# for
+for i in range(len(lista_frutas)):
+    print(lista_frutas[i])
+
+print()
+
+# for
+for fruta in lista_frutas:
+    print(fruta)
+
