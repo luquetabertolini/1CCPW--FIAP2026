0d5601dbe985577cc4d4b78064a658bf68708648
luquetabertolini <lubertolini1104@gmail.com>
2026-09-21 21:26:39 -0300

    Delete aula8/data directory

--- aula8/data/model.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-
-def model_lead(name, email, stage):
-    return {
-        "name": name,
-        "email": email,
-        "stage": stage,
-        "created": date.today().isoformat()
-    }
